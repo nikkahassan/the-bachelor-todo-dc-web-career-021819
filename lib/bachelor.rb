07583f93bacb91fdcf88139b83967b6a14c8919c
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-15 16:45:35 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,10 @@ def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant|
     contestant.each do |key, value|
       if value == "Winner"
-        return key[:name]
+        return key[:name].split(' ').first
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
