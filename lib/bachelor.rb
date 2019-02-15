dd440456bd5494ee7d81cee349e8732f8901315d
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-15 16:57:26 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,12 +1,3 @@
-data.each do |season, info| 
-   if info[:occupation].include?(name)
-       return season["name"]
-    end
-  end
-
-
-
-
 def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant|
     contestant.each do |key, value|
@@ -19,17 +10,7 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  data.each do |season, array|
-    array.each do |contestant|
-      people.each do |k,v|
-        if v == occupation
-          return people["name"]
-        end
-      end
-    end
-  end
- end
-end
+  
 
 def count_contestants_by_hometown(data, hometown)
   # code here
