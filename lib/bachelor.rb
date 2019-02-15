d9ab5ef4f382f0fc05616e3ea287aa20b8e6ab05
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-15 17:03:19 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -12,9 +12,14 @@ end
 def get_contestant_name(data, occupation)
   data.each do |season, info|
     info.each do |contestant|
-    
+      contestant[:occupation].each do |key, value|
         if value == occupation
           return contestant["name"]
+        end
+      end
+    end
+  end
+end
           
 
 def count_contestants_by_hometown(data, hometown)
